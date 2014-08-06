efcd513d57d5d10f41ca99e2cf20860466ff2510
elyervesson <elyervesson@gmail.com>
2014-08-06 13:19:33 -0300

    aula do laboratorio 1

---
+++ Labs/Laboratorio 01/Entrada_e_Saida_de_dados_usando_arquivos.R
@@ -0,0 +1,21 @@
+#Entrada e saida de dados usando arquivos
+
+#Comandos mais comuns para manipulação dos dados(entrada e saida)
+
+scan- Lê um arquivo texto e transforma o conteudo em um vetor
+      assumindo que cada elemento do arquivo esta separado 
+      espaço ou por uma nova linha
+
+read.csv- Lê um arquivo texto e transforma no formato csv e
+          e transforma o conteudo em uma tabela do tipo dataframe
+
+read.table - Lê um arquivo de texto em que cada elemento é separado
+             por um determinado caracter separador e transforma o 
+             conteuido em uma tabela do tipo dataframe.
+
+write - comando simples de escrita de dados em arquivos
+
+write.table - Escreve tabelas em um arquivo de saida, com varias opções
+              de formatação
+
+write.csv - Escreve tabelas em um arquivo de saida em formato CSV
